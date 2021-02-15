a61dafcd4583162e438e916cb7b3eac0fd8f0b0d
dneldik <dneldik@gmail.com>
2021-02-15 12:04:16 +0100

    Refactoring
    add class Controller
    move routing and action jobs to Controller class

--- index.php
+++ index.php
@@ -5,36 +5,11 @@ declare(strict_types=1);
 namespace App;
 
 require_once("src/Utils/debug.php");
-require_once("src/View.php");
+require_once("src/Controller.php");
 
-const DEFAULT_ACTION =  'list';
+$request = [
+  'get' => $_GET,
+  'post' => $_POST
+];
 
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-$view = new View();
-
-switch($action) {
-
-  case 'create':
-    $page = 'create';
-    $created = false;
-
-    if(!empty($_POST)) {
-      $created = true;
-      $viewParams = [
-        'title' => $_POST['title'],
-        'content' => $_POST['content'],
-      ];
-    }
-    $viewParams['created'] = $created;
-  break;
-
-  default:
-    $page = 'list';
-    $viewParams['elements'] = 'list of elements';
-  
-}
-
-$view->render($page, $viewParams);
-
+(new Controller($request))->run();
